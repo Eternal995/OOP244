b98bba65ea5fe5fbf3b3751bb7222c3a8dda9748
Eternal995 <longyongda@gmail.com>
2022-07-16 18:06:24 -0400

    Update Project MS2

--- project/ms2/LibApp.h
+++ project/ms2/LibApp.h
@@ -13,10 +13,28 @@ that my professor provided to complete my project milestones.
 #ifndef _SDDS_LIBAPP_H_
 #define _SDDS_LIBAPP_H_
 
+#include "Menu.h"
 #include <iostream>
 
 namespace sdds {
+    class LibApp {
+        bool m_changed;
+        Menu m_mainMenu{"Seneca Library Application"};
+        Menu m_exitMenu{"Changes have been made to the data, what would you like to do?"};
 
+        bool confirm(const char *message) const;
+        void load() const;
+        void save() const;
+        void search() const;
+        void returnPub();
+        void newPublication();
+        void removePublication();
+        void checkOutPub();
+
+    public:
+        LibApp();
+        void run();
+    };
 }
 
 #endif
